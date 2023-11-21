9c0b4c074aa42a17cc60af3977de94602291a449
josiah <josiah.chapman@gmail.com>
2023-11-20 16:51:10 -0800

    (~)

---
+++ src/cosmology.cpp
@@ -0,0 +1,5 @@
+#include "cosmology.h"
+
+namespace PROJECT_NAMESPACE {
+
+}
---
+++ src/paradigm.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <complex>
+#include <vector>
+#include <list>
+#define PROJECT_NAMESPACE cosmology
+
+////////////////////////
+/////////// helpful macros
+/////////////////
+#define up std::unique_ptr
+#define mkup std::make_unique
+#define sp std::shared_ptr
+#define mksp std::make_shared
+#define mv std::move
+#define vec std::vector
+#define lst std::list
+#define umap std::unordered_map
+#define scast static_cast
+#define scint scast<int>
+#define scflt scast<float>
+#define cmplx std::complex<double>
+
+///////////////////
+////// constants
+///////////
+namespace PROJECT_NAMESPACE {
+
+}
