52bc323533f3d6b9ec78ff359a742b873ee90fdf
josiah <josiah.chapman@gmail.com>
2023-11-21 12:32:32 -0800

    (~)

--- src/axioms/interfaces.h
+++ src/axioms/interfaces.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include "paradigm.h"
+#include <thread>
+#include "./time.h"
 
-namespace PROJECT_NAMESPACE {
+namespace cosmology {
 
 class Name {
 public:
@@ -12,6 +13,33 @@ public:
 
 protected:
     Name() = default;
+};
+
+class Circlet : public Name {
+public:
+    virtual ~Circlet() = default;
+
+    virtual void activate() = 0;
+
+    virtual bool finished() { return false; };
+
+    virtual uint64_t get_tick_interval() = 0;
+
+    static up<std::thread> begin(up<Circlet> circlet) {
+        auto thread = mkup<std::thread>(circle, mv(circlet));
+        return thread;
+    }
+
+    static void circle(up<Circlet> circlet) {
+        while (!circlet->finished()) {
+            auto cycle_start_time = get_current_time();
+            circlet->activate();
+            auto total_cycle_time = get_elapsed_time(cycle_start_time);
+            if (circlet->get_tick_interval() > total_cycle_time) {
+                sleep(circlet->get_tick_interval() - total_cycle_time);
+            }
+        }
+    }
 };
 
 }
