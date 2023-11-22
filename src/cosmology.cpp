24caf886b5ac513f5c974ad3f3c288b9a52c98cf
josiah <josiah.chapman@gmail.com>
2023-11-21 17:45:29 -0800

    cosmology

--- src/cosmology.cpp
+++ src/cosmology.cpp
@@ -12,7 +12,11 @@ Cosmology::Cosmology(int observation_width, int observation_height, int luon_cou
     OBSERVATION_HEIGHT = observation_height;
     LUON_COUNT = luon_count;
     psyche = mkup<Psyche>(LUON_COUNT);
-    impression = mkup<HappyPinkHearts>(*psyche);
+    vec<int> harmony_indices{};
+    for (int i = 0; i < LUON_COUNT; i++) {
+        harmony_indices.push_back(i);
+    }
+    impression = mkup<HappyPinkHearts>(psyche->create_harmony(harmony_indices));
 }
 
 void Cosmology::experience(sp<Signal<float>> &signal) {
--- src/impressions/happy_pink_hearts.h
+++ src/impressions/happy_pink_hearts.h
@@ -5,14 +5,46 @@
 
 namespace cosmology {
 
+
+class Heart {
+public:
+    Luon &luon;
+    Point origin;
+
+    Heart(Luon &luon, Point origin) : luon{luon}, origin{origin} {
+
+    }
+
+    void paint(Lattice &lattice) {
+        for (int y = origin.y - 3; y < origin.y + 3; y++) {
+            for (int x = origin.x - 3; x < origin.x + 3; x++) {
+                lattice.set_color(x, y, Color{255, 156, 215});
+            }
+        }
+    }
+};
+
 class HappyPinkHearts : public Name {
 private:
-    Psyche &psyche;
+    up<Harmony> harmony;
+    vec<Heart> hearts;
 
 public:
-    HappyPinkHearts(Psyche& psyche);
+    HappyPinkHearts(up<Harmony> harmony) : harmony{mv(harmony)} {
+        for (auto &luon: *this->harmony->luons) {
+            float x = Randomizer::generate(OBSERVATION_WIDTH);
+            float y = Randomizer::generate(OBSERVATION_HEIGHT);
+            hearts.emplace_back(*luon, Point{x, y});
+        }
+    }
 
-    up<Lattice> observe();
+    up<Lattice> observe() {
+        auto lattice = mkup<Lattice>(OBSERVATION_WIDTH, OBSERVATION_HEIGHT, Color{0, 0, 0});
+        for (auto &heart: hearts) {
+            heart.paint(*lattice);
+        }
+        return lattice;
+    }
 };
 
 }
