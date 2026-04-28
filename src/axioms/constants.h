615e69361f0d14c2bd5a149b01d94c7be7c3ec37
josiah <josiah.chapman@gmail.com>
2026-04-27 21:30:02 -0700

    (~)

--- src/axioms/constants.h
+++ src/axioms/constants.h
@@ -3,13 +3,13 @@
 #include <cstdint>
 
 namespace cosmology {
-
 extern int OBSERVATION_WIDTH;
 extern int OBSERVATION_HEIGHT;
-extern int LUON_COUNT;
-extern int DELTA_CHAIN_LENGTH;
 extern uint64_t WORLD_SEED[4];
+
+#ifdef threads_found
+extern int LUON_COUNT;
 extern int THREAD_COUNT;
 extern int RENDER_TIMEOUT_MICROSECONDS;
-
+#endif
 }
